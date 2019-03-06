33781659f39885a50174fac6365d015f90426b4b
Anna Moeller <anna.moeller@umontana.edu>
2019-03-05 17:15:11 -0700

    Fixed samp_freq.

--- R/sampling_start.R
+++ R/sampling_start.R
@@ -1,9 +1,9 @@
 #' Create sequence of start times
 #'
 #' Create a sequence of start times by defining the amount of time between each, in seconds.
-#' E.g., samp = 3600 for sampling once per hour.
+#' E.g., samp_freq = 3600 for sampling once per hour.
 #'
-#' @param samp The number of seconds between each sampling period
+#' @param samp_freq The number of seconds between each sampling period
 #' @param date_lim A vector of length 2 of class POSIXct. The first and last date of the
 #' desired sampling period.
 #'
@@ -15,14 +15,14 @@
 #' d <- c(Sys.time()-43200, Sys.time())
 #' sampling_start(3600, d)
 #'
-sampling_start <- function(samp, date_lim){
+sampling_start <- function(samp_freq, date_lim){
 
   # Make sure date_lim is the right dimension and class
   stopifnot(is.null(date_lim) | length(date_lim) == 2)
   stopifnot(class(date_lim) == c("POSIXct", "POSIXt"))
 
   # Vector of all the start times
-  s <- seq(from = date_lim[1], to = date_lim[2], by = paste(samp, "sec"))
+  s <- seq(from = date_lim[1], to = date_lim[2], by = paste(samp_freq, "sec"))
 
   # Take out the last time in s, because it will almost never be a full sampling period.
   s <- s[1:(length(s) - 1)]
