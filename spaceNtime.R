4b69a0c04c9ade1ccd119c1de8e3fc589da1e102
Anna Moeller <anna.moeller@umontana.edu>
2020-07-03 12:02:36 -0600

    Trying to document package

---
+++ spaceNtime.R
@@ -0,0 +1,10 @@
+#' spaceNtime: a package for estimating abundance of unmarked animals from 
+#' camera trap photos. 
+#' 
+#' The package provides functions for manipulating data and running the three 
+#' models developed by [Moeller et al. 2018](https://esajournals.onlinelibrary.wiley.com/doi/full/10.1002/ecs2.2331)
+#' 
+#' 
+#' @docType package
+#' @name spaceNtime
+NULL
