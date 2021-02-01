96ce3ae3da1da9718a23ab8f56761e9690713e63
Anna Moeller <anna.moeller@umontana.edu>
2021-02-01 14:37:56 -0600

    Added Kenneth as package author

--- R/spaceNtime-package.R
+++ R/spaceNtime-package.R
@@ -3,18 +3,28 @@
 #' 
 #' @description The package provides functions for manipulating data and running the three 
 #' models developed by 
-#' \href{https://esajournals.onlinelibrary.wiley.com/doi/full/10.1002/ecs2.2331}{Moeller et al. 2018}.
+#' [Moeller et al. 2018](https://esajournals.onlinelibrary.wiley.com/doi/full/10.1002/ecs2.2331).
 #' 
-#' @seealso Check out the vignette \code{browseVignettes("spaceNtime")} to see a 
-#' workflow for using this package. 
+#' @seealso Check out the vignettes in `browseVignettes("spaceNtime")` to see a 
+#' workflow for using this package and running goodness of fit. 
 #' 
-#' @author Anna Moeller
+#' 
+#' @author 
+#' 
+#' **Maintainer**: Anna Moeller [(ORCID)](https://orcid.org/0000-0001-9102-6959)
+#' 
+#' Authors: 
+#' 
+#' - Anna Moeller 
+#' 
+#' - Kenneth Loonam
+#' 
 #' @docType package
 #' @name spaceNtime
 #' @keywords internal
 #' @references Moeller, A. K., P. M. Lukacs, and J. Horne. 2018. Three novel methods
 #' to estimate abundance of unmarked animals using remote cameras. Ecosphere 9(8):
 #' e02331. \href{https://esajournals.onlinelibrary.wiley.com/doi/full/10.1002/ecs2.2331}{10.1002/ecs2.2331}
-#' 
+#' @md
 "_PACKAGE"
 NULL
